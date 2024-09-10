f674e7da4c94bbeb95b61d170d989ec7985d7468
Elisei <eli10es@yahoo.es>
2024-09-10 21:30:42 +0300

    fixed hover on links issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectsComponent } from './projects/projects.component';
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,SkillsComponent,ExperienceComponent,ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
