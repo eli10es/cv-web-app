ece028c43328f08f882953b611fdf8c73c76d229
Elisei <eli10es@yahoo.es>
2024-09-11 21:32:43 +0300

    skills,experience,projects etc

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.css'
+  styleUrl: './skills.component.css',
 })
 export class SkillsComponent {
+  skills = ['Angular', 'Javascript', 'C#', 'SQL'];
 
+  getRandomRotation() {
+    let randomVal = Math.random() * 30 - 15;
+    return `rotate(${randomVal}deg)`;
+  }
+
+  startDragging() {}
 }
